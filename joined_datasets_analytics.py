9df806d625b4a805a082ad71b62fda22f6c5202b
Mathilde <mps565@nyu.edu>
2021-11-18 23:16:21 -0500

    more statistics for the Analytics

--- joined_datasets_analytics.py
+++ joined_datasets_analytics.py
@@ -2,6 +2,7 @@ from pyspark.sql.types import StructType, StructField, StringType, FloatType, In
 from pyspark.sql.functions import *
 
 for year in range(2014, 2019):
+  print("Year: " + str(year))
   governmental_coverage_year_df = governmental_coverage_df.filter(governmental_coverage_df.year == year)
   governmental_coverage_year_df.show()
   governmental_coverage_year_df.count()
@@ -10,12 +11,31 @@ for year in range(2014, 2019):
   expenses_year_df = expenses_df.filter(expenses_df.year == str(year))
   expenses_year_df.show(10)
   expenses_year_df.count()
+  print("")
   
   df = governmental_coverage_year_df.join(expenses_year_df, expenses_year_df.countries == governmental_coverage_year_df.country, "inner")
   df = df.drop("countries")
   df = df.drop("year")
   
-  print("Year: " + str(year))
-  df.corr("governmental_coverage","che_pc_usd")  
-  df.describe("governmental_coverage","che_pc_usd").show()
+  df.corr("governmental_coverage","che_pc_usd")
+  
+  print("----------")
+  print("STATISTICS")
+  print("----------")
+  
+  statistics_df = df.describe("governmental_coverage","che_pc_usd")
+  statistics_df.show()
+  
+  min_val = statistics_df.where(col("summary") == "min").select("governmental_coverage").collect()[0][0]
+  print("Minimum value for government insurance coverage: " + min_val)
+  countries_min_val = df.where(col("governmental_coverage") == min_val).select("country", "income_group", "che_pc_usd")
+  print("Countries with the minimum value for governmental insurance coverage and their total health expenditure: ")
+  countries_min_val.show(200)
+  
+  max_val = statistics_df.where(col("summary") == "max").select("governmental_coverage").collect()[0][0]
+  print("Maximum value for government insurance coverage: " + max_val)
+  countries_max_val = df.where(col("governmental_coverage") == max_val).select("country", "income_group", "che_pc_usd")
+  print("Countries with the maximum value for governmental insurance coverage and their total health expenditure: ")
+  countries_max_val.show(200)
+  
   print("")
