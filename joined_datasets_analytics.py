37efb0cf3b0f7433d2cdef3ec8f3c74dde789794
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2021-11-18 19:35:51 -0500

    Update joined_datasets_analytics.py

--- joined_datasets_analytics.py
+++ joined_datasets_analytics.py
@@ -5,11 +5,11 @@ for year in range(2014, 2019):
   governmental_coverage_year_df = governmental_coverage_df.filter(governmental_coverage_df.year == year)
   governmental_coverage_year_df.show()
   governmental_coverage_year_df.count()
-
+  
   expenses_year_df = expenses_df.filter(expenses_df.year == str(year))
   expenses_year_df.show(10)
   expenses_year_df.count()
-
+  
   df = governmental_coverage_year_df.join(expenses_year_df, expenses_year_df.countries == governmental_coverage_year_df.country, "inner")
   df = df.drop("countries")
   df = df.drop("year")
