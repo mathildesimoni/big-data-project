7868ae3a5f445454ae5f202b43433fb7e4bb8eef
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2021-11-18 19:33:08 -0500

    Update joined_datasets_analytics.py

--- joined_datasets_analytics.py
+++ joined_datasets_analytics.py
@@ -1,16 +1,20 @@
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType
 from pyspark.sql.functions import *
 
-# 1st corelation for year 2018
-governmental_coverage_2018_df = governmental_coverage_df.filter(governmental_coverage_df.year == 2018)
-governmental_coverage_2018_df.show()
-governmental_coverage_2018_df.count()
+for year in range(2014, 2019):
+  governmental_coverage_year_df = governmental_coverage_df.filter(governmental_coverage_df.year == year)
+  governmental_coverage_year_df.show()
+  governmental_coverage_year_df.count()
 
-expenses_2018_df = expenses_df.filter((expenses_df.year == "2018"))
-expenses_2018_df.show(10)
-expenses_2018_df.count()
+  expenses_year_df = expenses_df.filter(expenses_df.year == str(year))
+  expenses_year_df.show(10)
+  expenses_year_df.count()
 
-df = governmental_coverage_2018_df.join(expenses_2018_df, expenses_2018_df.countries == governmental_coverage_2018_df.country, "inner")
-df = df.drop("countries")
-df = df.drop("year")
-df.corr("governmental_coverage","che_pc_usd")  
+  df = governmental_coverage_year_df.join(expenses_year_df, expenses_year_df.countries == governmental_coverage_year_df.country, "inner")
+  df = df.drop("countries")
+  df = df.drop("year")
+  
+  print("Year: " + str(year))
+  df.corr("governmental_coverage","che_pc_usd")  
+  df.describe("governmental_coverage","che_pc_usd").show()
+  print("")
